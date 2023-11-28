17e765ed514c9b0c05c6a116a94881272349e9c7
seb67760 <146946626+seb67760@users.noreply.github.com>
2023-11-28 09:50:13 +0100

    Update backend_api.py

--- backend_api.py
+++ backend_api.py
@@ -3,6 +3,10 @@ import pickle
 import pandas as pd
 #import numpy as np
 import nltk
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 app = Flask(__name__)
     
