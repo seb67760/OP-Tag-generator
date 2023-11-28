18d6b66820bea0056ad8ada5e41c1f32c64ee69b
seb67760 <146946626+seb67760@users.noreply.github.com>
2023-11-28 11:45:38 +0100

    Update words_classification.py

--- words_classification.py
+++ words_classification.py
@@ -18,7 +18,7 @@ def on_click():
 
     #req = requests.post("http://127.0.0.1:8080/predict", = text_input)
     
-    req = requests.post("https://github.com/seb67760/OP-Tag-generator/master/backend_api.py") #, text = text_input)
+    req = requests.post("https://github.com/seb67760/OP-Tag-generator/master/backend_api.py") , text = text_input)
     
     resultat = req.json()
     rec = resultat["predictions"]
